8c6fb5da48aa39ebc82f4822e8a4336a237f54ed
AyaFrSh-TwindleDum <aya.sh.fr@gmail.com>
2026-04-23 09:28:21 +0300

    Update schoolApp.py

--- schoolApp.py
+++ schoolApp.py
@@ -1,7 +1,20 @@
 import streamlit as st
-# streamlit/config.toml.
-[theme]
-backgroundColor = "#c2c395"
+
+# Define your color
+bg_color = "#c2c395"
+
+# Inject CSS with markdown
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {bg_color};
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.write("my superb website")
 st.header("the best website you will ever see")
 st.subheader("so what is this website...?")
